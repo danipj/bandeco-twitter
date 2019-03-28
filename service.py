27f869ac4973fd1930df9232e7f07842546123b0
Daniela Palumbo <d166301@dac.unicamp.br>
2019-03-28 18:04:37 -0300

    Update lambda service base on script

--- service.py
+++ service.py
@@ -1,18 +1,31 @@
 # -*- coding: utf-8 -*-
 import bandecoapi.api as bandeco
+from datetime import datetime
 import tweepy
 
 
 def handler(event, context):
+
+    consumer_key=''
+    consumer_secret=''
+    access_key=''
+    access_secret=''
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
 
     twt = tweepy.API(auth)
 
+    menus = ["lunch", "veglunch"]
+    refeicao = "O almoço"
+    if int(datetime.now().strftime("%H")) > 14:
+        menus = ["dinner", "vegdinner"]
+        refeicao = "A janta"
 
-    menus = ["lunch"]
     lunch = bandeco.get_menu(menus=menus)
-    twt.update_status('O almoço de hoje é: '+lunch['menu']['lunch'][:260])
+
+    # removes last part from menu because it's kinda useless and exceeds character count
+    #print(refeicao+' de hoje é:\n'+"\n".join(lunch['menu'][menus[0]].split('\n')[:-1]))
+    twt.update_status(refeicao+' de hoje é:\n'+"\n".join(lunch['menu'][menus[0]].split('\n')[:-1]))
+    twt.update_status(refeicao+' veg de hoje é:\n'+"\n".join(lunch['menu'][menus[1]].split('\n')[:-1]))
     return 'top'
-    
