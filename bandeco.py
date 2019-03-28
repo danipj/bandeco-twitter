ec45ab78b5c3b30a1c2bf904e7b9dcfbb4127698
Daniela Palumbo <d166301@dac.unicamp.br>
2019-03-28 17:49:33 -0300

    Updated script to get dinner and vegetarian options

--- bandeco.py
+++ bandeco.py
@@ -1,14 +1,27 @@
 import bandecoapi.api as bandeco
+from datetime import datetime
 import tweepy
+
+consumer_key=''
+consumer_secret=''
+access_key=''
+access_secret=''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 
+
 twt = tweepy.API(auth)
 
-
 menus = ["lunch", "veglunch"]
+refeicao = "O almoço"
+if int(datetime.now().strftime("%H")) > 14:
+	menus = ["dinner", "vegdinner"]
+	refeicao = "A janta"
+
 lunch = bandeco.get_menu(menus=menus)
-#print(lunch)
-twt.update_status('O almoço de hoje é:\n'+lunch['menu']['lunch'][:260])
-twt.update_status('O almoço vegetariano de hoje é:\n'+lunch['menu']['veglunch'][:248])
+
+# removes last part from menu because it's kinda useless and exceeds character count
+#print(refeicao+' de hoje é:\n'+"\n".join(lunch['menu'][menus[0]].split('\n')[:-1]))
+twt.update_status(refeicao+' de hoje é:\n'+"\n".join(lunch['menu'][menus[0]].split('\n')[:-1]))
+twt.update_status(refeicao+' veg de hoje é:\n'+"\n".join(lunch['menu'][menus[1]].split('\n')[:-1]))
